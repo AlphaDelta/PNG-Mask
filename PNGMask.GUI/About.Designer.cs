b75ead38e95b3d023826eb32aab5fa33de5471d2
AlphaDelta <admin@skid.pro>
2016-01-01 21:35:37 +1300

    Prepare v1.0.0-alpha.3

--- PNGMask.GUI/About.Designer.cs
+++ PNGMask.GUI/About.Designer.cs
@@ -88,7 +88,7 @@
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(79, 13);
             this.label2.TabIndex = 13;
-            this.label2.Text = "v1.0.0-alpha.2";
+            this.label2.Text = "v1.0.0-alpha.3";
             // 
             // label4
             // 
